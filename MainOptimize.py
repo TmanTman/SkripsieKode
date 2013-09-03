18a8aa21e64d38a6450b9c5dd1f3a4d92d066294
TmanTman <tielman.mailbox@gmail.com>
2013-09-03 16:49:37 +0200

    Basic form of solution

--- MainOptimize.py
+++ MainOptimize.py
@@ -2,12 +2,16 @@ import numpy
 import scipy.optimize as opt
 
 def objective(x):
-	return x[0]*x[1]
+	grid_usage = 0
+	for i in lights:
+		if ligths[i]+pv_in[i]+x[i] > 0:
+			grid_usage = grid_usage+lights[i]+pv_in[i]+x[i]
+	return grid_usage
 
 def constr1(x):
-	return 1 - (x[0]**2 + x[1]**2)
+	
 
-def constr2(x):
-	return x[1]
-
-opt.fmin_cobyla(objective, [0.0, 0.1], [constr1, constr2], rhoend=1e-7)
+lights  = array([50, 50, 75, 150, 100, 100, 80, 80, 120, 180, 120, 100])
+pv_in = array([0, 0, 0, 50, 150, 300, 400, 300, 150, 50, 0, 0])
+X0 = ones(12)*50
+opt.fmin_cobyla(objective, [0.0, 0.1], constr, rhoend=1e-7)
