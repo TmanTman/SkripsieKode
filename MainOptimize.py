cdd3f38742ca85c255792c15d2413a1105aa5f82
TmanTman <tielman.mailbox@gmail.com>
2013-09-05 12:00:28 +0200

    Non-working visualization added

--- MainOptimize.py
+++ MainOptimize.py
@@ -1,13 +1,16 @@
 from numpy import *
 import scipy.optimize as opt
+import matplotlib.pyplot as plt
 
-def objective(x):
-	lights  = [50, 50, 75, 150, 100, 100, 80, 80, 120, 180, 120, 100]
-	pv_in = [0, 0, 0, -50, -150, -300, -400, -300, -150, -50, 0, 0]
+def objective(x, lights, pv_in, fig, ax, line1):
 	grid_usage = 0
 	for i in xrange(len(lights)):
 		if lights[i]+pv_in[i]+x[i] > 0:
 			grid_usage = grid_usage+lights[i]+pv_in[i]+x[i]
+	##Graphing##
+	line1.set_ydata(x)	
+	fig.canvas.draw()
+	####
 	return grid_usage
 
 ##Boundaries are not implementable in this form
@@ -36,5 +39,12 @@ def constr(x):
 	#All tests passed successfully	
 	return 1 	
 
+##Init##
 X0 = [50, 50, 50, 50, 50, 50 ,50, 50, 50, 50, 50, 50]
-opt.fmin_cobyla(objective, X0, cons=(constr,), rhoend=1e-7)
+lights  = [50, 50, 75, 150, 100, 100, 80, 80, 120, 180, 120, 100]
+pv_in = [0, 0, 0, -50, -150, -300, -400, -300, -150, -50, 0, 0]
+##Graphing##
+fig, ax = plt.subplots()
+line1,=ax.plot(arange(12), X0, 'r')
+##Optimization
+opt.fmin_cobyla(objective, X0, cons=(constr,), args=(lights, pv_in, fig, ax, line1), consargs=(), rhoend=1e-7)
