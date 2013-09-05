b8e19c132274c7d0ad30817a1eee329c0e6cedce
TmanTman <tielman.mailbox@gmail.com>
2013-09-05 21:50:09 +0200

    Bar implementation successful

--- MainOptimize.py
+++ MainOptimize.py
@@ -2,13 +2,14 @@ import numpy as np
 import scipy.optimize as opt
 import matplotlib.pyplot as plt
 
-def objective(x, lights, pv_in, fig, ax, line1):
+def objective(x, lights, pv_in, fig, ax, timeslots):
 	grid_usage = 0
 	for i in xrange(len(lights)):
 		if lights[i]+pv_in[i]+x[i] > 0:
 			grid_usage = grid_usage+lights[i]+pv_in[i]+x[i]
 	##Graphing##
-	line1.set_ydata(x)	
+	ax.clear()
+	ax.bar(timeslots, x, bottom=0)	
 	fig.canvas.draw()
 	####
 	return grid_usage
@@ -33,15 +34,15 @@ X0 = np.array([50, 50, 50, 50, 50, 50 ,50, 50, 50, 50, 50, 50])
 lights  = [50, 50, 75, 150, 100, 100, 80, 80, 120, 180, 120, 100]
 pv_in = [0, 0, 0, -50, -150, -300, -400, -300, -150, -50, 0, 0]
 ##Graphing##
+timeslots = np.arange(12)
 plt.ion()
 fig, ax = plt.subplots()
 plt.xlim(0, 15)
 plt.ylim(0, 200)
 plt.xlabel('timeslot')
 plt.title('Energy use')
-bot = X0.min().round()
-line1=ax.bar(np.arange(12), X0, bottom=0)
+ax.bar(timeslots, X0, bottom=0)
 fig.canvas.draw()
 ##Optimization
-x = opt.fmin_cobyla(objective, X0, cons=(constr,), args=(lights, pv_in, fig, ax, line1), consargs=(), rhoend=1e-7)
+x = opt.fmin_cobyla(objective, X0, cons=(constr,), args=(lights, pv_in, fig, ax, timeslots), consargs=(), rhoend=1e-7)
 
