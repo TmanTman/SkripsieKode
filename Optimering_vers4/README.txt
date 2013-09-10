e57090ce9b126cdc5af55442a900ba96500e23d3
TielmanMacLinPartition <tielman.mailbox@gmail.com>
2013-09-10 18:52:42 +0200

    ClientServer and Matlab Opt added

---
+++ Optimering_vers4/README.txt
@@ -0,0 +1,9 @@
+Skripsie
+Tielman Nieuwoudt
+4 Augustus 2013
+
+This version takes a system with a light, pump, and PV panel.
+It improves the system by adding a battery with the ability to load and discharge.
+
+The extra variables are handled by simply adding them to the list of variables that we give to the patternsearch algorhythm.
+The function is optimized for cost.
---
+++ Optimering_vers4/optimization_print.m
@@ -0,0 +1,39 @@
+function stop = psplotbestf(optimvalues,flag)
+%PSPLOTBESTF PlotFcn to plot best function value.
+%   STOP = PSPLOTBESTF(OPTIMVALUES,FLAG) where OPTIMVALUES is a structure
+%   with the following fields:
+%              x: current point X
+%      iteration: iteration count
+%           fval: function value
+%       meshsize: current mesh size
+%      funccount: number of function evaluations
+%         method: method used in last iteration
+%         TolFun: tolerance on function value in last iteration
+%           TolX: tolerance on X value in last iteration
+%
+%   FLAG: Current state in which PlotFcn is called. Possible values are:
+%           init: initialization state
+%           iter: iteration state
+%           done: final state
+%
+%   STOP: A boolean to stop the algorithm.
+%
+%   See also PATTERNSEARCH, GA, PSOPTIMSET.
+
+
+%   Copyright 2003-2005 The MathWorks, Inc.
+%   $Revision: 1.1.6.1 $  $Date: 2009/08/29 08:25:12 $
+
+stop = false;
+switch flag
+    case 'init'
+        Pumpgraph = bar(optimvalues.x);
+        set(Pumpgraph,'Tag','Pump');
+        xlabel('Hours through the day','interp','none'); 
+        ylabel('Energy used per hour','interp','none')
+        title(['Pump energy usage'],'interp','none');
+    case 'iter'
+        Pumpgraph = findobj(get(gca,'Children'),'Tag','Pump');
+        newY = [optimvalues.x];
+        set(Pumpgraph, 'Ydata',newY);
+end
